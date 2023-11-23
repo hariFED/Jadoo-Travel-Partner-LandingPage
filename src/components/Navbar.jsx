452f022c9b4f47ae7f64fb51fd7f04ace61cc055
Hariharan <109363771+hariFED@users.noreply.github.com>
2023-11-24 01:33:13 +0530

    Animation Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,13 +9,13 @@ const Navbar = () => {
                 </div>
                 <div>
                     <ul className='flex flex-row items-center space-x-16 cursor-pointer'>
-                        <li className='p-4'>Desitnations</li>
-                        <li className='p-4'>Hotels</li>
-                        <li className='p-4'>Flights</li>
-                        <li className='p-4'>Bookings</li>
-                        <li className='p-4'>Login</li>
-                        <li className='px-8 py-2 border-2 border-black rounded-xl'><button className=''>Sign Up</button></li>
-                        <li className='p-4'>EN</li>
+                        <li className='p-4 duration-300 ease-in border-b-2 border-transparent ease hover:border-black'>Desitnations</li>
+                        <li className='p-4 duration-300 ease-in border-b-2 border-transparent ease hover:border-black'>Hotels</li>
+                        <li className='p-4 duration-300 ease-in border-b-2 border-transparent ease hover:border-black'>Flights</li>
+                        <li className='p-4 duration-300 ease-in border-b-2 border-transparent ease hover:border-black'>Bookings</li>
+                        <li className='p-4 duration-300 ease-in border-b-2 border-transparent ease hover:border-black'>Login</li>
+                        <li className='px-8 py-2 duration-300 border-2 border-black rounded-xl hover:scale-110 hover:bg-white'><button className=''>Sign Up</button></li>
+                        <li className='p-4 duration-300 ease-in border-b-2 border-transparent ease hover:border-black'>EN</li>
                     </ul>
                 </div>
             </div>
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -8,29 +8,36 @@ const Hero = () => {
         <>
 
             <div className={`bg-image h-screen`}>
-                <div>
+                <div className="hidden sm:block">
                     <Navbar />
                 </div>
-                <div className="flex items-center justify-center h-[86.2vh] px-48 mt-14">
-                    <div className="flex items-center justify-start w-1/2 h-full">
+                <div className="flex items-center justify-center sm:h-[86.2vh] sm:px-48 sm:mt-14 sm:flex-row flex-col">
+                    <div className="flex items-center justify-center h-full sm:justify-start sm:w-1/2">
                         <div className="flex flex-col gap-12">
-                            <h2 className="text-2xl font-bold text-[#F1765D]">BEST DESTINATONS AROUND THE WORLD</h2>
-                            <h1 className="text-8xl headingfont text-[#181E4B] leading-12">Travel, <span className="bg-[url('../assets/Decore.svg')]"> enjoy </span><br />
-                                and live a new <br />
-                                and full life
-                            </h1>
-                            <p className="text-lg font-medium leading-8 poppins">
-                                Built Wicket longer admire do barton vanity itself do in it.<br />
-                                Preferred to sportsmen it engrossed listening. Park gate <br />
-                                sell they west hard for the.
-                            </p>
+                            <motion.div animate={{ y: 0 }} initial={{ y: -2000 }} transition={{ duration: 1, delay: 0.8 }}  >
+                                <h2 className="sm:text-2xl font-bold text-[#F1765D]">BEST DESTINATONS AROUND THE WORLD</h2>
+                            </motion.div>
+                            <motion.div animate={{ x: 0, y: 0 }} initial={{ x: -1000 }} transition={{ duration: 0.8, delay: 0.5 }}>
+                                <h1 className="sm:text-8xl text-4xl headingfont text-[#181E4B] leading-12">Travel, <span className="bg-[url('../assets/Decore.svg')]"> enjoy </span><br />
+                                    and live a new <br />
+                                    and full life
+                                </h1>
+                            </motion.div>
+                            <motion.div animate={{ y: 0 }} initial={{ y: 2000 }} transition={{ duration: 1, delay: 0.8 }}  >
+                                <p className="text-lg font-medium leading-8 poppins">
+                                    Built Wicket longer admire do barton vanity itself do in it.<br />
+                                    Preferred to sportsmen it engrossed listening. Park gate <br />
+                                    sell they west hard for the.
+                                </p>
+                            </motion.div>
                             <div className="flex gap-10 text-sm sansfont">
                                 <button className="bg-[#F1A501] px-4 py-3 w-18 rounded-xl text-white ">Find our more</button>
                                 <button className="bg-[#DF6951] px-4 py-3 w-18 rounded-xl text-white"><span className="px-2 play-icon"></span>Play Demo</button>
                             </div>
+
                         </div>
                     </div>
-                    <div className="w-1/2">
+                    <div className="hidden sm:w-1/2 sm:block">
                         <motion.div initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{
                             duration: 0.8, delay: 0.5, ease: [0, 0.71, 0.2, 1.01]
                         }} >
